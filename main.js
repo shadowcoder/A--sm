50ef498d21e57f27751343500680a9fec6e1489b
bobbybee <j104@rocketmail.com>
2014-04-05 20:50:19 -0400

    Functions, bug fixes, preparation for if-else

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ var fs = require('fs');
 var expression = require('./parser');
 
 var assignment = /^\s*([a-z]+)\s*([\+\-\*\/]?)=\s*([^\n;]*)/;
+var functionCall = /^\s*([^\(\s]+)\(([^\)]*)\)(?:[^:]?)/;
+var functionDecl = /^([^\(\s]+)\(([^\)]*)\):/;
+var ifElse = /^\s*if \(([a-z0-9\[\]]*)\s*(==|>=|<=|&&|)\s*([^\)]*)\):([^?\n^]*)(?:\?\s*([^\n]*))?/;
 
 var lines = fs.readFileSync(process.argv[2]).toString().split('\n');
+
+var literalStrings = [];
 
 for(var i = 0; i < lines.length; ++i) {
     if(assignment.test(lines[i])) {
@@ -17,7 +22,7 @@ for(var i = 0; i < lines.length; ++i) {
                 
         console.log(eexp[0].join('\n'));
         
-        if(["cs","fs","ds","ss","gs","ef"].indexOf(reg)) {
+        if(["cs","fs","ds","ss","gs","es"].indexOf(reg) > -1) {
             console.log("push ax");
             console.log("mov ax, "+eexp[1][0]);
             console.log("mov "+reg+", ax");
@@ -25,6 +30,32 @@ for(var i = 0; i < lines.length; ++i) {
         } else {
             console.log("mov "+reg+", "+eexp[1][0]); 
         }
+    } else if(functionDecl.test(lines[i])) {
+        var declParams = lines[i].match(functionDecl);
+        console.log(declParams[1]+":");
+        
+        var dparams = declParams[2].split(",");
+        for(var g = dparams.length-1;g>-1; --g) {
+            console.log("pop "+dparams[g]);
+        }
+    } else if(functionCall.test(lines[i])) {
+        var callParams = lines[i].match(functionCall);
+        var params = callParams[2].split(",");
+        for(var f = params.length-1; f > -1; --f) {
+            var p = params[f].trim();
+            if(p[0] == '\"' && p[p.length-1] == '\"') {
+                literalStrings.push(p);
+                console.log("push literalString"+literalStrings.length);
+            } else {
+                console.log("push "+p);
+            }
+        }
+        console.log("call "+callParams[1]);
+    } else if(ifElse.test(lines[i]) {
+        
+    } else if(lines[i].trim() == "LITERALS") {
+        for(var q = 0; q < literalStrings.length; ++q) console.log("literalString"+q+" db '"+literalStrings[q]+"', 0");
+        literalStrings = [];
     } else {
         console.log(lines[i]);
     }
